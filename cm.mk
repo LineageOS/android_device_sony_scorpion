aca705e061fd7f2bb0fe37f59672dd75514ccadb
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2015-04-24 18:51:46 +0200

    update signature
    Change-Id: I65877127fe67774b7d4039c2ae7d14519132d3d2

--- cm.mk
+++ cm.mk
@@ -7,8 +7,8 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 $(call inherit-product, device/sony/scorpion/full_scorpion.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP621
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/SGP621/SGP621:4.4.4/23.0.1.A.0.167/WP_3Rw:user/release-keys
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="SGP621-user 4.4.4 23.0.1.A.0.167 WP_3Rw release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/SGP621/SGP621:5.0.2/23.1.A.0.690/4135415812:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="SGP621-user 5.0.2 23.1.A.0.690 4135415812 release-keys"
 
 PRODUCT_NAME := cm_scorpion
 PRODUCT_DEVICE := scorpion
